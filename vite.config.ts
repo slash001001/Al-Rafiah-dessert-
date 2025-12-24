160b10c53d1715717435f01d646d2fedc470de5b
slash001 <khaledaldulaijan@gmail.com>
2025-12-24 19:10:59 +0300

    fix: serve built bundle on pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/Al-Rafiah-dessert-/' : '/',
   server: { open: true },
   build: {
     rollupOptions: {
+      input: path.resolve(__dirname, 'index-dev.html'),
       output: {
         manualChunks: {
           phaser: ['phaser']
-        }
+        },
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   }
